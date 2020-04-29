2bd565027a7a4760f7ffa0289dbeaa117486a014
Glitch (pr0-source) <none>
2020-04-29 17:56:14 +0000

    + Konzept3

--- server.js
+++ server.js
@@ -24,10 +24,11 @@ app.get("/", (request, response) => {
 
 //UPLOADING
 const isUpload = req => {
-  return (
+  return; /*(
     req.headers["user-agent"] ===
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
-  );
+    process.env.PR0AGENT
+  );*/
+  req.headers(["sec-fetch-mode"]) !== void 0;
 };
 
 const DB = {
@@ -41,18 +42,41 @@ const DB = {
     file:
       "https://i.pinimg.com/originals/af/2e/50/af2e50f5458d5019831d53bc02634f0c.jpg",
     web: "https://www.deviantart.com/dlowell/art/Cmc-Matrix-527689606"
+  },
+  1588182495145: {
+    file:
+      "https://r4---sn-4g5ednsz.googlevideo.com/videoplayback?expire=1588204085&ei=1b2pXsyPJMW2gAeJu4n4Bw&ip=24.134.59.13&id=o-ACQYWBwLE4Y1TaPuWM0gJb61ax6VDf-gQwdhbXOuJOim&itag=22&source=youtube&requiressl=yes&mh=C6&mm=31%2C29&mn=sn-4g5ednsz%2Csn-4g5e6nzz&ms=au%2Crdu&mv=m&mvi=3&pl=18&initcwndbps=1130000&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=42.097&lmt=1577534065537890&mt=1588182392&fvip=4&c=WEB&txp=1306222&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=AJpPlLswRQIgccyAdZOoO5J3xDVCsBPhSUjIBDcS5pXQO2XRKTgwAA4CIQDrtHPDZck-JejW2EswsbDE5YCGaGD3Apmqj74ITOpCgA%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=ALrAebAwRQIhAJsAW_62-60MZ2nxK2zj6YkxNOMJcNChEmeqnwVHfFA9AiB3Kl8Ssj977eeDdwhZPjIpsq3H6IITcFRtWR5MALt_6w%3D%3D&host=r4---sn-4g5ednsz.googlevideo.com",
+    web: "https://www.youtube.com/watch?v=5lcdSm6v5EA"
   }
 };
 
+//bongo
+app.get("/*", (req, res, next) => {
+  console.debug(req.originalUrl);
+  next();
+});
+
+//CONCEPT1
+// ->/x
 app.get("/test", (req, res) => {
-  console.debug(req.headers["user-agent"]);
   res.redirect(isUpload(req) ? DB.TEST1.file : DB.TEST1.web);
 });
 
-//soos?id=333
+//CONCEPT2
+//-> /soos?id=x
 app.get("/soos", (req, res) => {
   const ID = req.query.id,
-        ENTRY= DB[ID];
+    ENTRY = DB[ID];
+  ID && ENTRY
+    ? [res.redirect(isUpload(req) ? DB[ID].file : DB[ID].web)]
+    : res.status(404) && res.end();
+});
+
+//CONCEPT3
+//-> /soos/x
+app.get("/soos/:id", (req, res) => {
+  const ID = req.params.id,
+    ENTRY = DB[ID];
   ID && ENTRY
     ? [res.redirect(isUpload(req) ? DB[ID].file : DB[ID].web)]
     : res.status(404) && res.end();
