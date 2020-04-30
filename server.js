4db3fa2e729308e49a24721e08306ad36aae9c48
Glitch (pr0-source) <none>
2020-04-30 07:26:48 +0000

    fix uploading

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ app.use(express.static("public"));
 
 //UPLOADING
 const isUpload = req => {
-  return; /*(
-    req.headers["user-agent"] ===
-    process.env.PR0AGENT
-  );*/
-  req.headers(["sec-fetch-mode"]) !== void 0;
+  return req.headers["referer"] !== void 0;
 };
 
 //todo: use actual mysql db for posts
@@ -31,7 +27,7 @@ const DB = {
   },
   1588182495145: {
     file:
-      "https://r4---sn-4g5ednsz.googlevideo.com/videoplayback?expire=1588204085&ei=1b2pXsyPJMW2gAeJu4n4Bw&ip=24.134.59.13&id=o-ACQYWBwLE4Y1TaPuWM0gJb61ax6VDf-gQwdhbXOuJOim&itag=22&source=youtube&requiressl=yes&mh=C6&mm=31%2C29&mn=sn-4g5ednsz%2Csn-4g5e6nzz&ms=au%2Crdu&mv=m&mvi=3&pl=18&initcwndbps=1130000&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=42.097&lmt=1577534065537890&mt=1588182392&fvip=4&c=WEB&txp=1306222&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=AJpPlLswRQIgccyAdZOoO5J3xDVCsBPhSUjIBDcS5pXQO2XRKTgwAA4CIQDrtHPDZck-JejW2EswsbDE5YCGaGD3Apmqj74ITOpCgA%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=ALrAebAwRQIhAJsAW_62-60MZ2nxK2zj6YkxNOMJcNChEmeqnwVHfFA9AiB3Kl8Ssj977eeDdwhZPjIpsq3H6IITcFRtWR5MALt_6w%3D%3D&host=r4---sn-4g5ednsz.googlevideo.com",
+      "https://r4---sn-4g5e6nzz.googlevideo.com/videoplayback?expire=1588252615&ei=Z3uqXuTnM5mngAfj74WIDA&ip=24.134.59.13&id=o-AC2X59LBnGRR-ZzgMaGtMKFA1LbdAk7ibnpvQ1PZIq8F&itag=22&source=youtube&requiressl=yes&mh=C6&mm=31%2C26&mn=sn-4g5e6nzz%2Csn-5hne6nsr&ms=au%2Conr&mv=m&mvi=3&pl=18&initcwndbps=1327500&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=42.097&lmt=1577534065537890&mt=1588230931&fvip=4&c=WEB&txp=1306222&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=AJpPlLswRAIgYUKVxUV41qj-r8CSI2PYz8MYFnxx8YyAFQCaBHkymXYCIA3UIDu5ZMS_ahaqoIbRpPQF_v_6pzNFQ4gzAtXNFEf5&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=ALrAebAwRQIhAKlZfNBAgutd8lCsV_Je0ouLc2RBV5ALz-Y0fTX-rVqrAiAuYLoqUlIirA6RSGFMgkLSw1T-fhYesqSxaOQA8RvTWQ%3D%3D&host=r4---sn-4g5e6nzz.googlevideo.com",
     web: "https://www.youtube.com/watch?v=5lcdSm6v5EA"
   }
 };
@@ -40,16 +36,16 @@ app.set("trust proxy", true);
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (req, res) => {
-      res.write(
-        fs
-          .readFileSync(`${__dirname}/views/index.html`, "utf8")
-          .replace("{{abs}}", `${req.protocol}://${req.host}`) //absolute base
-      ) && res.end();
+  res.write(
+    fs
+      .readFileSync(`${__dirname}/views/index.html`, "utf8")
+      .replace("{{abs}}", `${req.protocol}://${req.host}`) //absolute base
+  ) && res.end();
 });
 
 //bongo
 app.get("/*", (req, res, next) => {
-  const _soos = +req.originalUrl.split("soos/")[1];
+  const _soos = +req.originalUrl.split("/")[1];
   console.debug({
     IP: req.ip,
     url: `${req.protocol}//${req.headers.host}${req.originalUrl}`,
